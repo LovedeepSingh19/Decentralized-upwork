1190f590ee95f3faaccdc693c3ebe4a19e76a8f5
Lovedeep <gurugobind.sh@gmail.com>
2024-05-25 15:04:48 +0530

    Turbo repo added

---
+++ apps/backend/src/privateKey.ts
@@ -0,0 +1,3 @@
+
+export const privateKey = process.env.PRIVATE_KEY!;
+export const parrent_wallet_address = process.env.PARRENT_WALLET_ADD!;
---
+++ apps/user-frontend/utils/index.ts
@@ -0,0 +1,3 @@
+
+export const BACKEND_URL = "http://localhost:3001";
+export const CLOUDFRONT_URL = "https://d1z2gqyyoktsq9.cloudfront.net"
